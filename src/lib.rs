273a81019aaa788529c2e716bff38d1993fc1daa
Erkki Seppälä <flux@inside.org>
2023-05-20 18:16:10 +0300

    lib: removed commented code

--- src/lib.rs
+++ src/lib.rs
@@ -6,30 +6,15 @@ use std::sync::Arc;
 mod editor;
 mod xy_input;
 
-/// The time it takes for the peak meter to decay by 12 dB after switching to complete silence.
-//const PEAK_METER_DECAY_MS: f64 = 150.0;
-
-/// This is mostly identical to the gain example, minus some fluff, and with a GUI.
 pub struct Xygrid {
     params: Arc<XygridParams>,
 
     prev_sent_x_value: Option<f32>,
     prev_sent_y_value: Option<f32>,
-
-    // /// Needed to normalize the peak meter's response based on the sample rate.
-    // // peak_meter_decay_weight: f32,
-    // /// The current data for the peak meter. This is stored as an [`Arc`] so we can share it between
-    // /// the GUI and the audio processing parts. If you have more state to share, then it's a good
-    // /// idea to put all of that in a struct behind a single `Arc`.
-    // ///
-    // /// This is stored as voltage gain.
-    // //peak_meter: Arc<AtomicF32>,
 }
 
 #[derive(Params)]
 struct XygridParams {
-    /// The editor state, saved together with the parameter state so the custom scaling can be
-    /// restored.
     #[persist = "editor-state"]
     editor_state: Arc<IcedState>,
 
@@ -47,9 +32,6 @@ impl Default for Xygrid {
 
 	    prev_sent_x_value: None,
 	    prev_sent_y_value: None,
-
-            // peak_meter_decay_weight: 1.0,
-            // peak_meter: Arc::new(AtomicF32::new(util::MINUS_INFINITY_DB)),
         }
     }
 }
@@ -62,21 +44,6 @@ impl Default for XygridParams {
 	    x: FloatParam::new("X", 0.0, FloatRange::Linear { min: -1.0, max: 1.0 }),
 
 	    y: FloatParam::new("Y", 0.0, FloatRange::Linear { min: -1.0, max: 1.0 }),
-
-            // See the main gain example for more details
-            // gain: FloatParam::new(
-            //     "Gain",
-            //     util::db_to_gain(0.0),
-            //     FloatRange::Skewed {
-            //         min: util::db_to_gain(-30.0),
-            //         max: util::db_to_gain(30.0),
-            //         factor: FloatRange::gain_skew_factor(-30.0, 30.0),
-            //     },
-            // )
-            // .with_smoother(SmoothingStyle::Logarithmic(50.0))
-            // .with_unit(" dB")
-            // .with_value_to_string(formatters::v2s_f32_gain_to_db(2))
-            // .with_string_to_value(formatters::s2v_f32_gain_to_db()),
         }
     }
 }
@@ -128,12 +95,6 @@ impl Plugin for Xygrid {
         buffer_config: &BufferConfig,
         _context: &mut impl InitContext<Self>,
     ) -> bool {
-        // After `PEAK_METER_DECAY_MS` milliseconds of pure silence, the peak meter's value should
-        // have dropped by 12 dB
-        // self.peak_meter_decay_weight = 0.25f64
-        //     .powf((buffer_config.sample_rate as f64 * PEAK_METER_DECAY_MS / 1000.0).recip())
-        //     as f32;
-
         true
     }
 
@@ -143,33 +104,6 @@ impl Plugin for Xygrid {
         _aux: &mut AuxiliaryBuffers,
         context: &mut impl ProcessContext<Self>,
     ) -> ProcessStatus {
-        for channel_samples in buffer.iter_samples() {
-            let mut amplitude = 0.0;
-            let num_samples = channel_samples.len();
-
-            // let gain = self.params.gain.smoothed.next();
-            // for sample in channel_samples {
-            //     *sample *= gain;
-            //     amplitude += *sample;
-            // }
-
-            // // To save resources, a plugin can (and probably should!) only perform expensive
-            // // calculations that are only displayed on the GUI while the GUI is open
-            // if self.params.editor_state.is_open() {
-            //     amplitude = (amplitude / num_samples as f32).abs();
-            //     let current_peak_meter = self.peak_meter.load(std::sync::atomic::Ordering::Relaxed);
-            //     let new_peak_meter = if amplitude > current_peak_meter {
-            //         amplitude
-            //     } else {
-            //         current_peak_meter * self.peak_meter_decay_weight
-            //             + amplitude * (1.0 - self.peak_meter_decay_weight)
-            //     };
-
-            //     self.peak_meter
-            //         .store(new_peak_meter, std::sync::atomic::Ordering::Relaxed)
-            // }
-        }
-
 	while let Some(event) = context.next_event() {
 	    context.send_event(event);
 	}
@@ -213,11 +147,4 @@ impl ClapPlugin for Xygrid {
     ];
 }
 
-// impl Vst3Plugin for Xygrid {
-//     const VST3_CLASS_ID: [u8; 16] = *b"XygridAaAAaAaAaA";
-//     const VST3_SUBCATEGORIES: &'static [Vst3SubCategory] =
-//         &[Vst3SubCategory::Fx, Vst3SubCategory::Tools];
-// }
-
 nih_export_clap!(Xygrid);
-// nih_export_vst3!(Xygrid);
